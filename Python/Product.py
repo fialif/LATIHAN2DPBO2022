01dc89075ba7070552e8b1cf1350b60d99649688
fialif <74746311+fialif@users.noreply.github.com>
2022-02-17 21:40:26 +0700

    Add files via upload

---
+++ Python/Product.py
@@ -0,0 +1,17 @@
+class Product:
+
+    def __init__(self, id, p):
+        self.idProduct = id
+        self.price = p
+
+    def setIDProduct(self, id):
+        self.idProduct = id
+
+    def getIDProduct(self):
+        return self.getIDProduct
+
+    def setPrice(self, p):
+        self.price = p
+    
+    def getPrice(self):
+        return self.price
